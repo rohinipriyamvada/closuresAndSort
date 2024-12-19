fc15947b4ade9a22478920425135fab3cf757801
rohinipriyamvada <183710587+rohinipriyamvada@users.noreply.github.com>
2024-12-19 14:21:08 +0530

    added function to sort even and odd numbers

--- sort.js
+++ sort.js
@@ -3,6 +3,7 @@ const comparator = function (pivot) {
     if (a > pivot && b > pivot) {
       return b - a;
     }
+
     return a - b;
   }
 }
@@ -13,5 +14,24 @@ const order = function (array, pivot) {
   return array.sort(customSort);
 }
 
-console.log(order([3, 5, 7, 1, 9], 5)); //1, 3, 5, 9, 7
-console.log(order([1, 8, 3, 6, 5, 4, 7, 2, 9], 5)); //1, 2, 3, 4, 5, 9, 8, 7, 6
+// console.log(order([3, 5, 7, 1, 9], 5)); //1, 3, 5, 9, 7
+// console.log(order([1, 8, 3, 6, 5, 4, 7, 2, 9], 5)); //1, 2, 3, 4, 5, 9, 8, 7, 6
+
+const isEven = function (number) {
+  return (number & 1) === 0;
+}
+
+const comaparator2 = function (a, b) {
+  if (isEven(a) && !isEven(b)) {
+    return -1;
+  }
+
+  if (!isEven(a) && isEven(b)) {
+    return 1;
+  }
+
+  return a - b;
+}
+
+console.log([3, 8, 5, 6, 7, 1, 2, 9].sort(comaparator2)); 
+//2, 6, 8, 1, 3, 5, 7, 9
